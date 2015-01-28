573418008476082b9428cebd6276492dbf80aabe
ashokrane <ashokrane@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-28 10:25:51 +0000

    = 1.2 = * Now if you have no weekdays enabled for delivery, then all the weekdays will be enabled on the checkout page.
    git-svn-id: https://plugins.svn.wordpress.org/order-delivery-date/trunk@1077354 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- order_delivery_date.php
+++ order_delivery_date.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 /*
 Plugin Name: Order Delivery Date for WP e-Commerce
 Plugin URI: http://www.tychesoftwares.com/store/free-plugin/order-delivery-date-on-checkout/
 Description: This plugin allows customers to choose their preferred Order Delivery Date during checkout.
 Author: Ashok Rane
-Version: 1.1
+Version: 1.2
 Author URI: http://www.tychesoftwares.com/about
 Contributor: Tyche Softwares, http://www.tychesoftwares.com/
 */
-$wpefield_version = '1.0';
+$wpefield_version = '1.2';
 function wpefield_delivery_date()
 {
 	global $wpdb;
@@ -34,8 +34,33 @@ function wpefield_delivery_date()
 	
 	if($field_id != '')
 	{
+		
+	    wp_enqueue_script( 'jquery' );
+	    
+	    //if(get_option("first_install") != "TRUE")
+	    {
+		    wp_enqueue_script( 'jquery-ui-datepicker' );
+		    
+		    wp_enqueue_style( 'jquery-ui', "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/smoothness/jquery-ui.css" , '', '', false);
+		    
+		    wp_enqueue_script(
+				'initialize-datepicker.js',
+				plugins_url('/js/initialize-datepicker.js', __FILE__),
+				'',
+				'',
+				false
+			);
+	    }
+	    //http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/ui-lightness/jquery-ui.css
+	    
+	
+		/*if(get_option("first_install") != "TRUE"){
+			print('<script type="text/javascript" src="'.plugins_url().'/order-delivery-date/available-dates.js"></script>');
+		}
 		$display = '<link rel="stylesheet" type="text/css" href="' . plugins_url() . '/order-delivery-date/datepicker.css">
-		<script type="text/javascript" src="' . plugins_url() . '/order-delivery-date/datepicker.js"></script>
+		<script type="text/javascript" src="' . plugins_url() . '/order-delivery-date/datepicker.js"></script>';*/
+		
+		$display = '
 		<style>
 		.wpsc_checkout_form_'.$field_id.'
 		{
@@ -48,15 +73,17 @@ function wpefield_delivery_date()
 		</style>
 		<script type="text/javascript">
         jQuery(function() {
-			var formats = ["MM d, yyyy","MM d, yyyy"];
-			jQuery("#wpsc_checkout_form_'.$field_id.'").width("150px");
-			jQuery("#wpsc_checkout_form_'.$field_id.'").val("").datepick({dateFormat: formats[1], minDate:1});
-			jQuery("#wpsc_checkout_form_'.$field_id.'").parent().append("<small style=\'font-size:10px;\'>We will try our best to deliver your order on the specified date</small>");
+			var formats = ["MM d, yy","MM d, yy"];
+			jQuery("#wpsc_checkout_form_'.$field_id.'").width("250px");
+
+			jQuery("#wpsc_checkout_form_'.$field_id.'").val("").datepicker({dateFormat: formats[1], beforeShow: avd, beforeShowDay: chd});
+			jQuery("#wpsc_checkout_form_'.$field_id.'").parent().append("<small style=\'font-size:10px;float:left;\'>We will try our best to deliver your order on the specified date</small>");
         });
         </script>';
 		echo $display;
 	}
 }
+
 function wpefield_activate()
 {
 	global $wpdb;
@@ -74,7 +101,25 @@ function wpefield_activate()
 		$query = "INSERT INTO $wpefield__TABLE (`id`, `name`, `type`, `mandatory`, `display_log`, `default`, `active`, `checkout_order`, `unique_name`, `options`, `checkout_set`) VALUES
 ('', 'Delivery Date', 'text', '0', '0', '0', '1', $max_count, 'e_deliverydate', '', '0');";
 		$wpdb->query($query);
+		//update_option("first_install","TRUE");
 	}
+	
+	update_option('Monday_check','checked');
+	update_option('Tuesday_check','checked');
+	update_option('Wednesday_check','checked');
+	update_option('Thursday_check','checked');
+	update_option('Friday_check','checked');
+	update_option('Saturday_check','');
+	update_option('Sunday_check','');
+	update_option('Monday','on');
+	update_option('Tuesday','on');
+	update_option('Wednesday','on');
+	update_option('Thursday','on');
+	update_option('Friday','on');
+	update_option('Saturday','');
+	update_option('Sunday','');
+	update_option('orderDay','0');
+	update_option('availableDays','30');
 }
 function wpefield_deactivate()
 {
@@ -82,6 +127,24 @@ function wpefield_deactivate()
 	$wpefield__TABLE = $wpdb->prefix . 'wpsc_checkout_forms';
 	$query = "delete from $wpefield__TABLE where unique_name = 'e_deliverydate'";
 	$wpdb->query( $query );
+	
+	delete_option('Monday_check');
+	delete_option('Tuesday_check');
+	delete_option('Wednesday_check');
+	delete_option('Thursday_check');
+	delete_option('Friday_check');
+	delete_option('Saturday_check');
+	delete_option('Sunday_check');
+	delete_option('Monday');
+	delete_option('Tuesday');
+	delete_option('Wednesday');
+	delete_option('Thursday');
+	delete_option('Friday');
+	delete_option('Saturday');
+	delete_option('Sunday');
+	delete_option('orderDay');
+	delete_option('availableDays');
+	
 }
 add_action('wpsc_before_form_of_shopping_cart', 'wpefield_delivery_date');
 register_activation_hook( __FILE__, 'wpefield_activate' );
@@ -116,15 +179,35 @@ function order_delivery_date_front_scripts(){
 	$alldays['Sunday'] = get_option('Sunday');
 	
 	$alldayskeys = array_keys($alldays);
-	
-	foreach($alldayskeys as $key){
+	$checked = "No";
+	foreach($alldayskeys as $key)
+	{
+		if($alldays[$key] == 'on')
+		{
+			$checked = "Yes";
+		}
+	}
+	/*foreach($alldayskeys as $key){
 		print('<input type="hidden" id="'.$key.'" value="'.$alldays[$key].'">');
 		//print("Key: $key Value: $alldays[$key] <br />");
-	}	
+	}	*/
+	if($checked == 'Yes')
+	{
+		foreach($alldayskeys as $key)
+		{
+			print('<input type="hidden" id="'.$key.'" value="'.$alldays[$key].'">');
+		}
+	}
+	else if($checked == 'No')
+	{
+		foreach($alldayskeys as $key)
+		{
+			print('<input type="hidden" id="'.$key.'" value="on">');
+		}
+	}
 	print('<input type="hidden" name="orderDays" id="order-days" value="'.get_option('orderDay').'">');
 	print('<input type="hidden" name="availableDays" id="availableDays" value="'.get_option('availableDays').'">');
 	
-	print('<script type="text/javascript" src="'.plugins_url().'/order-delivery-date/available-dates.js"></script>');
 	print('<script type="text/javascript">
 				</script>');	
 }
@@ -136,54 +219,61 @@ function order_delivery_date_menu()
 	add_menu_page( 'Order Delivery Date','Order Delivery Date','administrator', 'order_delivery_date','order_delivery_date_settings');
 }
 function order_delivery_date_settings(){
-
+	
 	$check_prev = array();
 	
 /*	if(empty(get_option('Monday'))){
 			$check_prev['monday'] = ""; 
 	}*/
-	print('<br /><br />
-		<div id="order-delivery-date-settings">
+
+	print('<br>');
+	if(isset($_POST['save']) && $_POST['save'] != "")
+	{
+		print('<div id="message" class="updated"><p>All changes have been saved.</p></div>');
+	}
+	print('<br>');
+
+	print('<div id="order-delivery-date-settings">
 			<div class="ino_titlee"><h3><span class="home">Order Delivery Date Settings</span></h3></div>
 				<form id="order-delivery-date-settings-form" name="order-delivery-date-settings" method="post">
 					
 					<div id="order-days">
-						<label for="delivery-days-tf">Delivery Days: </label>
+						<label  class="orddd_label" for="delivery-days-tf">Delivery Days: </label>
 						<fieldset class="days-fieldset">
 							<legend><b>Days:</b></legend>
 								<input type="checkbox" name="Monday" id="Monday" class="day-checkbox" '.get_option('Monday_check').' />
-								<label for="Monday">Monday</label>
+								<label  class="orddd_label" for="Monday">Monday</label>
 								<br />
 								<input type="checkbox" name="Tuesday" id="Tuesday" class="day-checkbox" '.get_option('Tuesday_check').' />
-								<label for="Tuesday">Tuesday</label>
+								<label  class="orddd_label" for="Tuesday">Tuesday</label>
 								<br />
 								<input type="checkbox" name="Wednesday" id="Wednesday" class="day-checkbox" '.get_option('Wednesday_check').' />
-								<label for="Wednesday">Wednesday</label>
+								<label  class="orddd_label" for="Wednesday">Wednesday</label>
 								<br />
 								<input type="checkbox" name="Thursday" id="Thursday" class="day-checkbox" '.get_option('Thursday_check').' />
-								<label for="Thursday">Thursday</label>
+								<label  class="orddd_label" for="Thursday">Thursday</label>
 								<br />
 								<input type="checkbox" name="Friday" id="Friday" class="day-checkbox" '.get_option('Friday_check').' />
-								<label for="Friday">Friday</label>
+								<label  class="orddd_label" for="Friday">Friday</label>
 								<br />
 								<input type="checkbox" name="Saturday" id="Saturday" class="day-checkbox" '.get_option('Saturday_check').' />
-								<label for="Saturday">Saturday</label>
+								<label  class="orddd_label" for="Saturday">Saturday</label>
 								<br />
 								<input type="checkbox" name="Sunday" id="Sunday" class="day-checkbox" '.get_option('Sunday_check').' />
-								<label for="Sunday">Sunday</label>
+								<label  class="orddd_label" for="Sunday">Sunday</label>
 								<br />
 						</fieldset>
 						<div id="help">Select the week days when the delivery of items takes place. For example, if you deliver only on Tuesday, Wednesday, Thursday & Friday, then select only those days here. The remaining days will not be available for selection to the customer.</div>
 					</div>
 
 					<div id="order-delay-days">
-						<label for="order-delay-days-tf">Minimum Delivery time (in days): </label>
+						<label  class="orddd_label" for="order-delay-days-tf">Minimum Delivery time (in days): </label>
 						<input type="text" name="orderday" id="orderday" value="'.get_option('OrderDay').'"/>
 						<div id="help" >Enter the minimum number of days it takes for you to deliver an order. For example, if it takes 2 days atleast to ship an order, enter 2 here. The customer can select a date that is available only after the minimum days that are entered here.<br></div>
 					</div>
 
 					<div id="available-days">
-						<label for="available-days-tf">Number of dates to choose: </label>
+						<label  class="orddd_label" for="available-days-tf">Number of dates to choose: </label>
 						<input type="text" name="available-days-tf" id="available-days-tf" value="'.get_option('availableDays').'"/>
 						<div id="help">Based on the above 2 settings, you can decide how many dates should be made available to the customer to choose from. For example, if you enter 10, then 10 different dates will be made available to the customer to choose.</div>
 					</div>
@@ -192,48 +282,115 @@ function order_delivery_date_settings(){
 				</form>
 			</div>');
 }
-if($_POST['save']){
-	update_option('Monday',$_POST['Monday']);	
-	update_option('Tuesday',$_POST['Tuesday']);	
-	update_option('Wednesday',$_POST['Wednesday']);	
-	update_option('Thursday',$_POST['Thursday']);	
-	update_option('Friday',$_POST['Friday']);	
-	update_option('Saturday',$_POST['Saturday']);	
-	update_option('Sunday',$_POST['Sunday']);	
-	update_option('orderDay',$_POST['orderday']);
-	update_option('availableDays',$_POST['available-days-tf']);
+if(isset($_POST['save']))
+{
+	//update_option("first_install","FALSE");
+	if(isset($_POST['Monday']))
+	{
+		update_option('Monday',$_POST['Monday']);	
+	}
+	else
+	{
+		update_option('Monday','');
+	}
+	if(isset($_POST['Tuesday']))
+	{
+		update_option('Tuesday',$_POST['Tuesday']);
+	}
+	else
+	{
+		update_option('Tuesday','');	
+	}
+	if(isset($_POST['Wednesday']))
+	{
+		update_option('Wednesday',$_POST['Wednesday']);		
+	}
+	else
+	{
+		update_option('Wednesday','');	
+	}
+	if(isset($_POST['Thursday']))
+	{
+		update_option('Thursday',$_POST['Thursday']);	
+	}
+	else
+	{
+		update_option('Thursday','');	
+	}
+	if(isset($_POST['Friday']))
+	{
+		update_option('Friday',$_POST['Friday']);	
+	}
+	else
+	{
+		update_option('Friday','');
+	}
+	if(isset($_POST['Saturday']))
+	{
+		update_option('Saturday',$_POST['Saturday']);	
+	}
+	else
+	{
+		update_option('Saturday','');
+	}
+	if(isset($_POST['Sunday']))
+	{
+		update_option('Sunday',$_POST['Sunday']);
+	}
+	else
+	{
+		update_option('Sunday','');
+	}
+
+	if(isset($_POST['orderday']))
+	{
+		update_option('orderDay',$_POST['orderday']);
+	}
+	else
+	{
+		update_option('orderDay','');
+	}
+	
+	if(isset($_POST['available-days-tf']))
+	{
+		update_option('availableDays',$_POST['available-days-tf']);
+	}
+	else
+	{
+		update_option('availableDays','');
+	}
 	//checked property
-	if($_POST['Monday'] == "on")	
+	if(isset($_POST['Monday']) && $_POST['Monday'] == "on")	
 		update_option('Monday_check','checked="checked"');	
 	else
 		update_option('Monday_check','');	
 		
-	if($_POST['Tuesday'] == "on")	
+	if(isset($_POST['Tuesday']) && $_POST['Tuesday'] == "on")	
 		update_option('Tuesday_check','checked="checked"');	
 	else
 		update_option('Tuesday_check','');	
 
-	if($_POST['Wednesday'] == "on")	
+	if(isset($_POST['Wednesday']) && $_POST['Wednesday'] == "on")	
 		update_option('Wednesday_check','checked="checked"');	
 	else
 		update_option('Wednesday_check','');	
 
-	if($_POST['Thursday'] == "on")	
+	if(isset($_POST['Thursday']) && $_POST['Thursday'] == "on")	
 		update_option('Thursday_check','checked="checked"');	
 	else
 		update_option('Thursday_check','');	
 	
-	if($_POST['Friday'] == "on")	
+	if(isset($_POST['Friday']) && $_POST['Friday'] == "on")	
 		update_option('Friday_check','checked="checked"');	
 	else
 		update_option('Friday_check','');	
 
-	if($_POST['Saturday'] == "on")	
+	if(isset($_POST['Saturday']) && $_POST['Saturday'] == "on")	
 		update_option('Saturday_check','checked="checked"');	
 	else
 		update_option('Saturday_check','');	
 
-	if($_POST['Sunday'] == "on")	
+	if(isset($_POST['Sunday']) && $_POST['Sunday'] == "on")	
 		update_option('Sunday_check','checked="checked"');	
 	else
 		update_option('Sunday_check','');	
--- readme.txt
+++ readme.txt
@@ -1,31 +1,42 @@
 === Order Delivery Date for WP e-Commerce ===
 Contributors: ashokrane
-Tags: delivery date, checkout, order delivery
+Tags: delivery date, checkout, order delivery, calendar, checkout calendar, datepicker
 Requires at least: 3.2
 Tested up to: 3.4
-Stable tag: 1.1
+Stable tag: trunk
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Donate link: http://www.tychesoftwares.com/
 
 Allow the customers to choose an order delivery date on the checkout page for WP e-Commerce store owners.
 
 == Description ==
 
-This plugin will allow the customer to choose an order delivery date on the checkout page. The customer can choose any delivery date that is after the current date. 
+This plugin will allow the customer to choose an order delivery date on the checkout page. The customer can choose any delivery date that is after the current date. The plugin uses the inbuilt datepicker that comes with WordPress.
 
 The delivery date chosen by the customer will be visible to the site administrator while viewing the order under the "Additional Checkout Fields" section.
 
-This plugin allows you to improve your customer service by delivering the order on the customer's specified date.
+Order Delivery Date settings in the Admin panel allow the site administrator to have control over aspects like setting the order delivery week days, minimum delivery time (in days) and number of dates to offer to the user to choose from.
 
-Other free plugins for WP e-Commerce:
+This plugin allows you to improve your customer service by delivering the order on the customer's chosen date.
 
-* [Viral Coupon Lite for WP e-Commerce](http://wordpress.org/extend/plugins/viral-coupon-for-wp-e-commerce-lite "Viral Coupon Plugin") - Social Media Promotion Tool for your store (Facebook)
-* [Email Domain Checker](http://wordpress.org/extend/plugins/email-domain-checker) - Validates the domain name of the email address entered at checkout
+**Pro Version:**
 
+**[Order Delivery Date Pro 2.1](http://www.tychesoftwares.com/store/premium-plugins/order-delivery-date-for-wp-e-commerce-pro "Order Delivery Date Pro")** - Version 2.1 allows the customer to choose a delivery date & time on the checkout page. Date Settings, Time Settings, Appearance & Black-out dates allow the site owner to decide which dates should be made available for delivery. What's new in 2.1?
+<ol>
+<li>Ability to allow the customer to select <strong>Delivery Time along with Delivery Date</strong></li>
+<li><strong>Same-day & Next-day delivery</strong> with cut-off time</li>
+<li>Choose from <strong>24 different themes for the calendar</strong></li>
+<li>Specify the time range available for delivery / pick up</li>
+<li><strong>Add holidays or black-out dates</strong> to the calendar</li>
+<li>Option to <strong>show Delivery Date in Customer Notification Email</strong></li>
+<li>Show 2 months in calendar</li>
+<li>Choose the convenient date format</li>
+<li><strong>Customize field label, field note</strong> text</li>
+<li>Capture only delivery date or only delivery time or both</li>
+</ol>
 
-**Premium plugins for WP e-Commerce:**
-
-* **[Viral Coupon Pro](http://codecanyon.net/item/viral-coupon-for-wp-ecommerce/2644621 "Viral Coupon Pro")** - Promotes your store on Twitter & Google+ in addition to Facebook.
+**[View Demo](http://www.tychesoftwares.com/store/premium-plugins/order-delivery-date-for-wp-e-commerce-pro "View Demo")**
 
 == Installation ==
 
@@ -39,21 +50,33 @@ Other free plugins for WP e-Commerce:
 
 = Can the customer enter the preferred order delivery time? =
 
-Currently there is no provision for entering the delivery time. This is planned in future release.
+Currently there is no provision for entering the delivery time in the free version. This is possible in the Version 2.1. [View Demo](http://www.tychesoftwares.com/store/premium-plugins/order-delivery-date-for-wp-e-commerce-pro "View Demo")
 
 = Is the order delivery date field mandatory on checkout page? =
 
-No. The delivery date field is optional.
+No. The delivery date field is optional in the free version. The field can be configured as Mandatory or optional in the Version 2.1. [View Demo](http://www.tychesoftwares.com/store/premium-plugins/order-delivery-date-for-wp-e-commerce-pro "View Demo")
 
 == Screenshots ==
 
-1. The Delivery date field will be visible on checkout page, with past dates disabled.
+1. The Delivery date field will be visible on checkout page, with dates displayed as per the settings.
 
 2. The order delivery date settings in the Admin Panel for greater control.
 
 3. The selected delivery date will be shown in "Additional Checkout Fields" in Sales Log.
 
+
 == Changelog ==
 
 = 1.0 =
 * Initial release.
+
+= 1.1 =
+* Added order delivery settings that allows the site owner to have better control over the dates that will be displayed to the customer. Settings include available order delivery days, minimum days required for order processing and number of dates to show to the customer to choose.
+
+= 1.2 =
+* Now if you have no weekdays enabled for delivery, then all the weekdays will be enabled on the checkout page.
+
+== Upgrade Notice ==
+
+= 1.1 =
+Newly added order delivery date settings allow the site owner to have control on dates to be presented to the customer for delivering the order.
